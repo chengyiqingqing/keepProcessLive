555da3359c6c3ae089cd257274184a933d9f7e15
shaowenwen <shaowenwen@amberweather.com>
2017-12-29 15:55:21 +0800

    finish it

--- keeplive/src/main/java/com/henrik/keeplive/MainActivity.java
+++ keeplive/src/main/java/com/henrik/keeplive/MainActivity.java
@@ -21,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart: " );
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         Log.e(TAG, "onStart: " );
